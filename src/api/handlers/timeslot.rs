cc78ac1d744d4c67d084cdd1f6a263e4b2950705
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-09-10 16:14:37 +0200

    MVP of exporting finished

--- src/api/handlers/timeslot.rs
+++ src/api/handlers/timeslot.rs
@@ -1,15 +1,24 @@
+use std::collections::BTreeMap;
+use std::fmt::Write;
 use std::ops::Range;
 
+use anyhow::Context;
 use axum::http::StatusCode;
-use chrono::{Datelike, NaiveDate, NaiveTime, Weekday};
+use chrono::{Datelike, NaiveDate, NaiveTime, Weekday, IsoWeek};
 use chrono_tz::Tz;
+use futures_util::{FutureExt, StreamExt};
+use futures_util::stream::FuturesUnordered;
 use mongodb::Database;
 use serde::Deserialize;
 use uuid::Uuid;
 
+use crate::api::logic::check_object_belong_to_userid;
+use crate::api::logic::entry::get_time_from_index_and_timeslot;
+use crate::api::logic::timeslot::get_index_range_timeslot;
 use crate::api::util::prelude::*;
 use crate::auth::UserId;
-use crate::db::model::{BsonTimeSlot, Student, TimeSlot};
+use crate::db::model::{BsonTimeSlot, Student, TimeSlot, Entry, HasUserId};
+use crate::db::queries::entry::get_entry_by_index_range;
 use crate::db::queries::timeslot::{get_timeslot_by_id, get_timeslots, insert_timeslot};
 use crate::util::create_isoweek;
 
@@ -57,6 +66,20 @@ pub async fn create(
 		));
 	}
 
+	if r.timerange.start > r.timerange.end {
+		return Ok(NotFine(
+			StatusCode::UNPROCESSABLE_ENTITY,
+			"timerange start should be before end"
+		))
+	}
+
+	if r.time.start > r.time.end {
+		return Ok(NotFine(
+			StatusCode::UNPROCESSABLE_ENTITY,
+			"start time should be before end"
+		))
+	}
+
 	let id = Uuid::new_v4();
 	let ts = BsonTimeSlot {
 		user_id: u.0.clone(),
@@ -75,14 +98,11 @@ pub async fn create(
 }
 
 #[derive(Deserialize)]
-struct IsoWeekYear {
-	year: i32,
-	week: u32,
-}
-
-#[derive(Deserialize)]
 pub struct ExportRequest {
-	range: Range<IsoWeekYear>,
+	start_year: i32,
+	start_week: u32,
+	end_year: i32,
+	end_week: u32,
 	#[serde(default)]
 	allow_incomplete_week: bool,
 }
@@ -90,17 +110,65 @@ pub struct ExportRequest {
 pub async fn export(
 	u: UserId,
 	db: Database,
-	r: ExportRequest,
+	q: ExportRequest,
 ) -> anyhow::Result<WebResult<String, &'static str>> {
-	let Some(start) = create_isoweek(r.range.start.year, r.range.start.week) else {
+	let Some(start) = create_isoweek(q.start_year, q.start_week) else {
 		return Ok(WebResult::NotFine(StatusCode::UNPROCESSABLE_ENTITY, "invalid week/year"));
 	};
 
-	let Some(end) = create_isoweek(r.range.end.year, r.range.end.week) else {
+	let Some(end) = create_isoweek(q.end_year, q.end_week) else {
 		return Ok(WebResult::NotFine(StatusCode::UNPROCESSABLE_ENTITY, "invalid week/year"));
 	};
 
-	let ts = get_timeslots(db.clone(), u.clone()).await?;
+	let user_timeslots = get_timeslots(db.clone(), u.clone()).await?;
 
-	todo!()
+	if let WebResult::NotFine(c, e) = check_object_belong_to_userid(user_timeslots.iter(), &u) {
+		return Ok(WebResult::NotFine(c, e))
+	}
+
+	let index_ranges = user_timeslots.into_iter()
+		.map(|ts| get_index_range_timeslot(&ts, start..end).map(|r| (r, ts)));
+
+	let mut timeslot_handles = Vec::new();
+
+	for i in index_ranges {
+		match i {
+			Some(r) => {
+				let db_task = db.clone();
+				let u_task = u.clone();
+				timeslot_handles.push(tokio::spawn(get_entry_by_index_range(db_task, u_task, r.1.id, r.0).map(|res| res.map(|e| (e, r.1)))));
+			},
+			None => return Ok(WebResult::NotFine(StatusCode::UNPROCESSABLE_ENTITY, "invalid timerange")),
+		}
+	}
+
+	let entry_results = FuturesUnordered::from_iter(timeslot_handles.into_iter()).collect::<Vec<_>>().await;
+
+	// BtreeMap, because we need ordering
+	let mut week_map: BTreeMap<IsoWeek, Vec<Entry>> = BTreeMap::new();
+	
+	for res in entry_results {
+		let (entries, ts) = res.unwrap()?;
+
+		for e in entries {
+			let iso_week = get_time_from_index_and_timeslot(&ts, e.index).context(format!("unable to get time from from entry: {}", e.identifier()))?.iso_week();
+
+			if let Some(week_entries) = week_map.get_mut(&iso_week) {
+				week_entries.push(e);
+			} else {
+				week_map.insert(iso_week, [e].into());
+			}
+		}
+	}
+
+	let mut output = String::new();
+
+	for (w, entries) in week_map.iter() {
+		writeln!(output, "KW{}", w.week())?;
+		for e in entries {
+			writeln!(output, "Unterricht ist super gelaufen mit {:?}", e.state)?;
+		}
+	}	
+
+	Ok(WebResult::Fine(StatusCode::OK, output))
 }
--- src/api/logic/timeslot.rs
+++ src/api/logic/timeslot.rs
@@ -1,9 +1,25 @@
 use std::ops::Range;
 
-use chrono::IsoWeek;
+use chrono::{IsoWeek, NaiveDate, Weekday};
 
 use crate::db::model::TimeSlot;
 
-pub fn get_index_range_timeslot(ts: &TimeSlot, range: Range<IsoWeek>) -> Range<u32> {
-	todo!()
+pub fn get_index_range_timeslot(ts: &TimeSlot, range: Range<IsoWeek>) -> Option<Range<u32>> {
+	let start = NaiveDate::from_isoywd_opt(range.start.year(), range.start.week(), Weekday::Mon)?;
+	let end = NaiveDate::from_isoywd_opt(range.end.year(), range.end.week(), Weekday::Sun)?;
+
+	if start < ts.timerange.start {
+		return None;
+	}
+
+	if end > ts.timerange.end {
+		return None;
+	}
+
+	// Both should always be positive
+	// Since ts.timerange.start <= ts.timerange.end is checked for in creation function.
+	let start_index: u32 = (start - ts.timerange.start).num_weeks().try_into().ok()?;
+	let end_index: u32 = (end - ts.timerange.start).num_weeks().try_into().ok()?;
+
+	Some(start_index..end_index)
 }
