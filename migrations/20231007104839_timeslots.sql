2399c584ea1f155f2a506639e5cfd725f00bd2d4
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-10-07 13:24:10 +0200

    fix migrations

--- migrations/20231007104839_timeslots.sql
+++ migrations/20231007104839_timeslots.sql
@@ -1,16 +1,16 @@
 -- Add migration script here
 CREATE TYPE student AS (
-	name varchar(255),
+	name varchar(255)
 );
 
 CREATE TYPE timeslot_time AS (
-	start time,
-	end time,
+	start_time	time,
+	end_time	time
 );
 
 CREATE TYPE timeslot_range AS (
-	start date,
-	end date,
+	start_date	date,
+	end_date	date
 );
 
 CREATE TABLE timeslots (
@@ -20,18 +20,13 @@ CREATE TABLE timeslots (
 	students student[] NOT NULL,
 	time timeslot_time NOT NULL,
 	timerange timeslot_range NOT NULL,
-	timezone varchar(127) NOT NULL,
-);
-
-CREATE TYPE student_state AS (
-	student student,
-	state smallint,
+	timezone varchar(127) NOT NULL
 );
 
 CREATE table entries (
+	id uuid PRIMARY KEY,
 	user_id varchar(255) NOT NULL,
 	index integer NOT NULL,
 	timeslot_id uuid NOT NULL REFERENCES timeslots(id) ON DELETE CASCADE,
-	status smallint NOT NULL,
-	student_states student_state[],
+	state jsonb NOT NULL
 );
