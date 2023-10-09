a4e36a79c520d567465c2f415e7eb67da9dce9bd
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-10-09 19:23:32 +0200

    use explicit name for unique index

--- migrations/20231009171307_fix_entry_unique_index.sql
+++ migrations/20231009171307_fix_entry_unique_index.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
 DROP INDEX entries_unique_index_index;
 
-CREATE UNIQUE INDEX ON entries (index, timeslot_id);
+CREATE UNIQUE INDEX entries_index_timeslot_id_idx ON entries (index, timeslot_id);
