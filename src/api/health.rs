40f508466422cbd681ca1432482eb0a5cb633f05
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-01-30 17:52:12 +0100

    convert health-check to handler-free design

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,11 +1,41 @@
 use axum::extract::State;
+use axum::http::StatusCode;
 
-use super::handlers::health;
-use super::AppState;
-use crate::api::util::{TransposeResult, WebResult};
+use sqlx::PgPool;
+
+use tracing::error;
+
+use crate::api::AppState;
+use crate::api::util::prelude::*;
+
+pub enum HealthCheckError {
+	DatabaseUnavailable,
+}
+
+impl From<HealthCheckError> for WebError<&'static str> {
+	fn from(v: HealthCheckError) -> WebError<&'static str> {
+		use HealthCheckError::*;
+		match v {
+			DatabaseUnavailable => (StatusCode::SERVICE_UNAVAILABLE, "database unavailable").into(),
+		}
+	}
+}
+
+pub async fn database_test(db: &PgPool) -> Result<(), ()> {
+	if let Err(e) = sqlx::query!("SELECT (1) as test").fetch_optional(&db).await {
+		error!(err=%e, "health check error");
+		return Err(());
+	}
+
+	Ok(())
+}
 
 pub async fn health_check(
 	State(AppState { db, .. }): State<AppState>,
 ) -> WebResult<&'static str, &'static str> {
-	health::health_check(db).await.transpose_web()
+	if database_test(&db).await.is_err() {
+		return Err(HealthCheckError::DatabaseUnavailable)?;
+	}
+
+	Ok("healthy".into())
 }
